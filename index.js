8b984760b69d5aac078be5f6cc6fc8b9b40b0a95
Cezar <pczampieri@hotmail.com>
2022-08-19 10:48:27 -0300

    Initial version

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const fs = require('fs')
 
 const bancosdados = require('./bancosdados.json')
 
-const bancosReady = bancosdados.map(({COMPE, ISPB, LongName, ShortName}) => ({COMPE, ISPB, LongName, ShortName}))
+const bancosReady = bancosdados.map(({COMPE, ISPB, LongName, ShortName}) => ({COMPE, ISPB, LongName, ShortName, BANK: `${COMPE} - ${LongName}` }))
 
 fs.writeFile('bancos.json', JSON.stringify(bancosReady, null, 2), {
   encoding: 'utf-8', flag: 'w'
 }, () => {
   console.log('ok!')
 } )
-
-console.log(bancosReady)
